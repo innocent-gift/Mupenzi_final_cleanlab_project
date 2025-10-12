1e6700f23aca58e42c394ccb7854490d8e5b9487
innocent-gift <i.nkurunziz@alustudent.com>
2025-10-12 15:52:20 +0200

    Fixed static file

--- with_wp/backend/backend.js
+++ with_wp/backend/backend.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // ✅ Serve frontend folder
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Database configuration
 const dbConfig = {
